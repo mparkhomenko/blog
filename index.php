226c6905d1f5b3299d5a60b0fba6d8a1135f6ba3
maksim <maksim@maksim.localhost>
2017-04-23 16:14:54 +0300

    add project

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+    <title>Bootstrap 101 Template</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/main.css">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <h1>Linux blog</h1>
+        <div class="navbar navbar-inverse">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" name="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
+                <span class="sr-only">Открыть навигацию</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a href="#" class="navbar-brand">
+                <img src="/images/tux.png" class="brand-image" alt="tux">
+              </a>
+            </div>
+            <div class="collapse navbar-collapse" id="responsive-menu">
+              <ul class="nav navbar-nav">
+                <li><a href="#">Блоги</a></li>
+                <li><a href="#">Поиск</a></li>
+              </ul>
+              <div class="navbar-form navbar-right">
+                <div class="form-group">
+                  <input type="text" name="userEmail" placeholder="E-mail" value="" class="form-control">
+                </div>
+                <div class="form-group">
+                  <input type="password" name="userPass" placeholder="Password" value="" class="form-control">
+                </div>
+                <button type="submit" name="buttonLogin" class="btn btn-primary">
+                  <i class="fa fa-sign-in" aria-hidden="true"></i> Войти
+                </button>
+                <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#modal-1">Зарегистрироваться</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="article col-lg-8 col-md-8 col-sm-6 col-xs-4">
+          <h2>Заголовок статьи <small>theme1</small> <small>theme2</small> <small>theme3</small> <small>theme4</small></h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div class="star-block">
+            <i class="fa fa-star"></i> <span class="star-count">100</span>
+          </div>
+        </div>
+
+        <div class="article col-lg-8 col-md-8 col-sm-6 col-xs-4">
+          <h2>Заголовок статьи <small>theme1</small> <small>theme2</small> <small>theme3</small> <small>theme4</small></h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div class="star-block">
+            <i class="fa fa-star"></i> <span class="star-count">100</span>
+          </div>
+        </div>
+
+        <div class="article col-lg-8 col-md-8 col-sm-6 col-xs-4">
+          <h2>Заголовок статьи <small>theme1</small> <small>theme2</small> <small>theme3</small> <small>theme4</small></h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div class="star-block">
+            <i class="fa fa-star"></i> <span class="star-count">100</span>
+          </div>
+        </div>
+
+        <div class="article col-lg-8 col-md-8 col-sm-6 col-xs-4">
+          <h2>Заголовок статьи <small>theme1</small> <small>theme2</small> <small>theme3</small> <small>theme4</small></h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div class="star-block">
+            <i class="fa fa-star"></i> <span class="star-count">100</span>
+          </div>
+        </div>
+
+        <div class="article col-lg-8 col-md-8 col-sm-6 col-xs-4">
+          <h2>Заголовок статьи <small>theme1</small> <small>theme2</small> <small>theme3</small> <small>theme4</small></h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div class="star-block">
+            <i class="fa fa-star"></i> <span class="star-count">100</span>
+          </div>
+        </div>
+
+        <div class="article col-lg-8 col-md-8 col-sm-6 col-xs-4">
+          <h2>Заголовок статьи <small>theme1</small> <small>theme2</small> <small>theme3</small> <small>theme4</small></h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div class="star-block">
+            <i class="fa fa-star"></i> <span class="star-count">100</span>
+          </div>
+        </div>
+        <ul class="pagination">
+          <li class="disabled"><a href="#">«</a></li>
+          <li class="active"><a href="#">1</a></li>
+          <li><a href="#">2</a></li>
+          <li><a href="#">3</a></li>
+          <li><a href="#">4</a></li>
+          <li><a href="#">5</a></li>
+          <li><a href="#">»</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="modal" id="modal-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Регистрация</h4>
+          </div>
+          <div class="modal-body">
+            <div class="form-horizontal">
+              <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <button type="submit" class="btn btn-default">Sign in</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button class="btn btn-danger" type="button" data-dismiss="modal">Закрыть</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
